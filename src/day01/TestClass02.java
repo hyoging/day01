a36fd78c8dc312d158a7f29ebe28c7393287e46d
test <a@a.com>
2023-06-12 15:27:10 +0900

    day01실습

---
+++ src/day01/TestClass02.java
@@ -0,0 +1,25 @@
+package day01;
+
+public class TestClass02 {
+
+	public static void main(String[] args) {
+		
+//		escape
+//		-문자열 안에서 특수한 기능을 한다.
+//		\n : 엔터값과 비슷
+//		\t : tab 크기만큼 오른쪽으로 커서 이동
+//		\" : (큰 따옴표) 하나로 표현
+//		\\ : 역슬러시 하나 표현
+		
+		System.out.println("안\n녕\n하\n세요");
+		System.out.println("1\t1234567\t12345678\t1");
+		System.out.println("이름\t나이");
+		System.out.println("홍길동\t20살");
+		
+		System.out.println("   \"  \\ ");
+		System.out.println("C:\\0.핀테크");
+		
+		
+	}
+
+}
